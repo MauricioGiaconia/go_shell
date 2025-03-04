f32ba19d063c438efba3e9504f5f0e867445c9df
Mauricio Giaconia <maurigiaconia@hotmail.com>
2025-03-04 06:05:47 -0300

    feat: logic to print current path

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -13,8 +13,14 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
+	currentPath, err := os.Getwd()
+
+	if err != nil {
+		currentPath = ""
+	}
+
 	for {
-		fmt.Print("Let's GO $ ")
+		fmt.Print(fmt.Sprintf("\033[38;2;0;255;255m%s\033[0m Let's GO $ ", currentPath))
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
 
